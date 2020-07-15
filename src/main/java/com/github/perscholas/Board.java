2b2f770d92bcf07b41392cf3e4b707beacbaf105
Laura Godinez <issagodinez@Lauras-MacBook-Pro.local>
2020-07-15 15:50:48 -0400

    Changes Made

--- src/main/java/com/github/perscholas/Board.java
+++ src/main/java/com/github/perscholas/Board.java
@@ -4,23 +4,55 @@ package com.github.perscholas;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][]board;
     public Board(Character[][] matrix) {
+        this.board=matrix;
+
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return isInFavorOf("X");
     }
 
+
+
+
     public Boolean isInFavorOfO() {
-        return null;
+        return isInFavorOf("O");
     }
 
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfO() && !isInFavorOfX();
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfX()) {
+            return "X";
+        } else if(isInFavorOfO()){
+                return "O";
+            }return "";
+        }
+
+    private Boolean isInFavorOf(String x) {
+        if (board[0][0].equals(x) && board[0][1].equals(x) && board[0][2].equals(x)) {
+            return true;
+        } else if (board[1][0].equals(x) && board[1][1].equals(x) && board[1][2].equals(x)) {
+            return true;
+        } else if (board[2][0].equals(x) && board[2][1].equals(x) && board[2][2].equals(x)) {
+            return true;
+        } else if (board[0][1].equals(x) && board[1][1].equals(x) && board[2][1].equals(x)) {
+            return true;
+        } else if (board[0][2].equals(x) && board[1][2].equals(x) && board[2][2].equals(x)) {
+            return true;
+        } else if (board[0][0].equals(x) && board[1][0].equals(x) && board[2][0].equals(x)) {
+            return true;
+        } else if(board[0][0].equals(x) && board[1][1].equals(x) && board[2][2].equals(x)) {
+            return true;
+        } else if(board[2][0].equals(x) && board[1][1].equals(x) && board[0][2].equals(x)) {
+            return true;
+        }else {
+            return false;
+        }
     }
 
 }
